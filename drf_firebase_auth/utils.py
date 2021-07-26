f18fa87f585f92ba6ddf3c9fab862746cef449f7
Ahmane Samir <samirahmane.trading@gmail.com>
2021-07-26 10:23:38 +0100

    feat: added get_firebase_user_id function
    This function takes a firebase_user object and returns its Firebase ID.

--- drf_firebase_auth/utils.py
+++ drf_firebase_auth/utils.py
@@ -5,6 +5,14 @@ import uuid
 from firebase_admin import auth
 
 
+def get_firebase_user_uid(firebase_user: auth.UserRecord) -> str:
+    try:
+        if firebase_user.uid:
+            return firebase_user.uid
+    except Exception as e:
+        raise Exception(e)
+
+
 def get_firebase_user_email(firebase_user: auth.UserRecord) -> str:
     try:
         return (
